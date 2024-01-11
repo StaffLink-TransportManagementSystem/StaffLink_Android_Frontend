3a286535efdebdb60174cb089e9a0601f2bbb8e1
Ravindu Hasanka <rhatu2000@gmail.com>
2024-01-11 22:43:59 +0530

    Add Drivers page created

---
+++ app/src/main/java/com/example/stafflinkmobile/add_driver_page1.kt
@@ -0,0 +1,11 @@
+package com.example.stafflinkmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_driver_page1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_driver_page1)
+    }
+}
