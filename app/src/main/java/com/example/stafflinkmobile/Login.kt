07739e6f80ecbd71ef0933e40b2aa9b024badfaa
Ravindu Hasanka <rhatu2000@gmail.com>
2024-01-13 16:00:24 +0530

    vehicle details page started

--- app/src/main/java/com/example/stafflinkmobile/Login.kt
+++ app/src/main/java/com/example/stafflinkmobile/Login.kt
@@ -2,11 +2,25 @@ package com.example.stafflinkmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val button: Button = findViewById(R.id.LoginButton)
+        button.setOnClickListener {
+            setContentView(R.layout.activity_dashboard)
+        }
+
+
+        val registerButton: TextView = findViewById(R.id.goToRegister)
+        registerButton.setOnClickListener {
+            setContentView(R.layout.activity_register)
+        }
     }
+
 
 }
