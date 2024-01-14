597a0bee5a53e05f402b276e9eeab55d74ab2ac9
Ravindu Hasanka <rhatu2000@gmail.com>
2024-01-14 21:33:07 +0530

    Passenger Details page

---
+++ app/src/main/java/com/example/stafflinkmobile/Passenger_Details.kt
@@ -0,0 +1,11 @@
+package com.example.stafflinkmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Passenger_Details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passenger_details)
+    }
+}
