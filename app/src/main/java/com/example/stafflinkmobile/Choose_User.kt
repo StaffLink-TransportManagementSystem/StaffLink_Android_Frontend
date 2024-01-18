f3344259e430448646a8078ba981e1bfef33443e
Ravindu Hasanka <rhatu2000@gmail.com>
2024-01-18 14:38:23 +0530

    choose user page

---
+++ app/src/main/java/com/example/stafflinkmobile/Choose_User.kt
@@ -0,0 +1,11 @@
+package com.example.stafflinkmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Choose_User : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_user)
+    }
+}
