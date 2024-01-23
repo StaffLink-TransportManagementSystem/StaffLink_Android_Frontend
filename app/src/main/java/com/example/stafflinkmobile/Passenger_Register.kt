793ba7ef3e9381232cd00d24b8597358188a8ef4
Ravindu Hasanka <rhatu2000@gmail.com>
2024-01-23 10:37:34 +0530

    passenger register started

---
+++ app/src/main/java/com/example/stafflinkmobile/Passenger_Register.kt
@@ -0,0 +1,11 @@
+package com.example.stafflinkmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Passenger_Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passenger_register)
+    }
+}
